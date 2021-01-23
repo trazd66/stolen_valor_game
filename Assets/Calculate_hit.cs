1812b4d2ed8fc2971b14c1a01c21a568cb5c6ee9
trazd66 <tdengzy3@gmail.com>
2021-01-23 14:31:56 -0500

    adding test model

---
+++ Assets/Calculate_hit.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calculate_hit : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Rigidbody player_rb;
+    public Rigidbody enemy_rb;
+
+    public int player_health = 100;
+    public int enemy_health = 100;
+    
+    bool collide = false;
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate(){
+        if(collide){
+            enemy_health -= 10;
+        }
+        collide = false;
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.attachedRigidbody == enemy_rb){
+            collide = true;
+        }
+    }
+}
