30c3391164054b2ecea5b25d6716e5a8b72da06c
alexb <alexandre.nunes@mail.utoronto.ca>
2021-04-08 13:56:34 -0400

    merge bug fixes

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -8,6 +8,18 @@ using Game_Control;
 public class MainMenu : MonoBehaviour
 {
 
+    public Slider theme_volume;
+    public Slider sfx_volume;
+
+    public AudioManager audio_manager;
+
+    void Awake()
+    {
+        audio_manager = GameObject.Find("AudioManager").GetComponent<AudioManager>();
+
+        theme_volume.onValueChanged.AddListener(delegate { audio_manager.SetThemeVolume(theme_volume.value); });
+        sfx_volume.onValueChanged.AddListener(delegate { audio_manager.SetSFXVolume(sfx_volume.value); });
+    }
 
     public void PlayGame()
     {
